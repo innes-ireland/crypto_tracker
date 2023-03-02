ba36551acbec3d080416f13d6ae3e43c528d3712
Innes Ireland <innesmireland@gmail.com>
2023-03-02 09:12:15 +0000

    modal window with info

--- src/Components/ModalWindow.jsx
+++ src/Components/ModalWindow.jsx
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+
 export default function ModalWindow({ setModalState, setModalData, modalData }) {
+    console.log(modalData)
+
+    const [graphData, setGraphData] = useState([])
+    const [graphPoints, setGraphPoints] = useState([])
+    const [graphApi, setGraphApi] = useState(`https://api.coingecko.com/api/v3/coins/${modalData.id}/market_chart?vs_currency=usd&days=7&interval=daily`)
+
+    const fetchGraphData = async () => {
+        const response = await axios.get(graphApi)
+        setGraphData(response.data)
+
+    }
+
+    const populateGraph = () => {
+        graphData.map((dataPoint) => { })
+
+
+    }
+    useEffect(() => { fetchGraphData() }, [graphApi])
+    console.log(graphData.prices)
+
 
     return (
         < div className="coinModal_wrapper" >
@@ -6,7 +30,15 @@ export default function ModalWindow({ setModalState, setModalData, modalData }) 
                 <h2> {modalData.name} </h2>
             </div>
             <div className="coinModal_content">
-                <h2> </h2>
+                <p> Current Price:{modalData.current_price}<br></br>
+                    Number in Circulation: {modalData.circulating_supply}<br></br>
+                    24hr high: {modalData.high_24h}<br></br>
+                    24hr low: {modalData.low_24h}<br></br>
+
+
+
+                </p>
+
             </div>
             <div className="closeButton">
                 <button className="closeButton" onClick={() => { { setModalData(null); setModalState(false) } }}>
