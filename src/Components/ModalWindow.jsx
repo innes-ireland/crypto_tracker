9e2178a41db3f7cb26a24ae9f846f3be571c9ea9
Innes Ireland <innesmireland@gmail.com>
2023-03-08 13:07:27 +0000

    writing function to retrieve data from nested arrays

--- src/Components/ModalWindow.jsx
+++ src/Components/ModalWindow.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 
 export default function ModalWindow({ setModalState, setModalData, modalData }) {
-    console.log(modalData)
+
 
     const [graphData, setGraphData] = useState([])
     const [dataSets, setDataSets] = useState({
@@ -14,9 +14,20 @@ export default function ModalWindow({ setModalState, setModalData, modalData }) 
 
     const [graphApi, setGraphApi] = useState(`https://api.coingecko.com/api/v3/coins/${modalData.id}/market_chart?vs_currency=usd&days=7&interval=daily`)
 
+    // fetch request for historic data to enable making line graph 
+
+    const fetchGraphData = async () => {
+        const response = await axios.get(graphApi)
+        setGraphData(response.data.prices)
+
+    }
+
+
+    // function to retrieve last seven days in date format for x axis points
+
     const getLastSevenDays = () => {
         const days = []
-        const milliSecondsPerDay = 86400000;
+        const milliSecondsPerDay = 86400000; // 864000 = 24 * 3600 * 1000
 
         for (let i = 6; i >= 0; i--) {
             const day = new Date(Date.now() - i * milliSecondsPerDay);
@@ -27,65 +38,48 @@ export default function ModalWindow({ setModalState, setModalData, modalData }) 
 
     const lastSevenDays = getLastSevenDays()
 
+    // function to retrieve coin prices from nested array that is graphData // 
+    console.log(graphData)
+    console.log(graphData.length)
+    function getCoinPrices(graphData) {
+        const coinPrices = []
+        for (var i = 0; i < graphData.length; i++) {
+            console.log(graphData[i])
+            const innerArray = graphData[i];
+            {
+                const price = innerArray[1];
+                coinPrices.push(price)
+
+            }
+            return coinPrices
+
+        }
 
 
-    const fetchGraphData = async () => {
-        const response = await axios.get(graphApi)
-        setGraphData(response.data)
+    }
+    console.log(getCoinPrices(graphData))
 
+
+
+    // object used to feed data to reactChartJS component
+    const data = {
+        labels: lastSevenDays,
+        dataSets: {
+            labels: "price tracking",
+            data: [1, 2, 3, 4, 5, 6, 7],
+            backgroundColor: "#0080F6",
+        }
     }
 
 
+
+
+
+
+
     useEffect(() => { fetchGraphData() }, [graphApi])
-    console.log(graphData.prices)
 
 
-
-    useEffect(() => {
-
-
-
-        let temp_arr = [] // temp_arr allows for continual updating of list as opposed to reseting with each new label added to chart
-        let label_arr = lastSevenDays
-        let colors_arr = []
-        graphData.forEach((priceArray) => {
-            console.log(priceArray[1])
-
-
-
-
-
-
-
-
-
-        },)
-
-
-        setDataSets(dataSetsprev => {
-
-            return ({
-                ...dataSetsprev,
-                data: temp_arr,
-                backgroundColor: colors_arr,
-                borderColor: colors_arr,
-            });
-
-        })
-
-        console.log(dataSets)
-        setLabels(label_arr)
-
-        setGraphData({
-            labels: label_arr,
-            datasets: [dataSets]
-        })
-
-
-
-
-
-    }, [])
 
 
     return (
@@ -103,6 +97,7 @@ export default function ModalWindow({ setModalState, setModalData, modalData }) 
 
 
                     </p>
+
 
                 </div>
                 <div className="closeButton">
